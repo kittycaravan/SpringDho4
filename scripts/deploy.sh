fe5a8fb5a4ed18b2093d6c28341cd011a7ef7ca9
kaminus <nyang4@daum.net>
2025-06-07 20:29:29 +0900

    ci cd 디플로이 에서 에러나서 스크립트 다시 수정

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Deploying WAR..."
+cp /home/ec2-user/app/dho4.war /opt/tomcat9/webapps/ROOT.war
+exit 0
+
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,9 @@
-#!/bin/bash		
-TOMCAT_DIR=/opt/tomcat9		
-		
-$TOMCAT_DIR/bin/shutdown.sh		
-sleep 5		
-rm -rf $TOMCAT_DIR/webapps/dho4*		
+#!/bin/bash
+TOMCAT_DIR=/opt/tomcat9
+
+echo "Stopping Tomcat..."
+$TOMCAT_DIR/bin/shutdown.sh || true
+sleep 5
+rm -rf $TOMCAT_DIR/webapps/ROOT*
+exit 0
+
