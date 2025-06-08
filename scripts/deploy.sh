eddb8e44033186b7652c20ce782d54c780f19ad7
kaminus <nyang4@daum.net>
2025-06-08 12:42:37 +0900

    되라 좀

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-echo "Deploying WAR..."
-cp /home/ec2-user/app/dho4.war /opt/tomcat9/webapps/ROOT.war
-exit 0
+#!/bin/bash						
+echo "Removing existing WAR file..."						
+rm -f /home/ec2-user/app/dho4.war						
+						
+echo "Deploying WAR..."						
+cp /home/ec2-user/app/dho4.war /opt/tomcat9/webapps/ROOT.war						
+exit 0						
